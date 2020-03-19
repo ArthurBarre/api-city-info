deb6e94bc322b0e5ed651c4d325b63f7580ffcca
Arthur Barré <arthur.barre@easy.movie>
2020-03-19 10:57:30 +0100

    change url to get all fields od town

--- src/Service/EtablissementPublicApi.php
+++ src/Service/EtablissementPublicApi.php
@@ -5,7 +5,7 @@ class EtablissementPublicApi
 {
     public function getTownHall($code) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://etablissements-publics.api.gouv.fr/v3/departements/".$code."/mairie");
+        curl_setopt($ch, CURLOPT_URL, "https://geo.api.gouv.fr/communes?code=".$code."&fields=nom,code,codesPostaux,centre,surface,contour,codeDepartement,departement,codeRegion,region,population&format=json&geometry=centre");
         $res = curl_exec($ch);
         $result = json_decode($res,true);
         return $result;
